4ea3c7af212fca5df889a2b1760c92f31d1457dc
swsk33 <yanhanhui2001a@163.com>
2021-05-06 20:30:37 +0800

    v1.0.0

--- WebPages/static/js/index.js
+++ WebPages/static/js/index.js
@@ -1,1 +1,45 @@
-//
+let currentIndex = 0;
+let ableToScroll = true;
+
+//鼠标滚轮事件
+window.addEventListener('wheel', (e) => {
+	if (ableToScroll) {
+		let h = window.innerHeight;
+		if (e.deltaY > 0) {
+			if (currentIndex < 4) {
+				currentIndex++;
+			}
+		} else {
+			if (currentIndex > 0) {
+				currentIndex--;
+			}
+		}
+		window.scrollTo({
+			top: currentIndex * 0.93 * h,
+			behavior: 'smooth'
+		});
+		ableToScroll = false;
+	}
+	setTimeout(() => {
+		ableToScroll = true;
+	}, 500);
+});
+
+//上栏按钮
+let navButtons = document.querySelector('header ul').children;
+for (let i = 0; i < navButtons.length; i++) {
+	navButtons[i].addEventListener('click', () => {
+		if (ableToScroll) {
+			let h = window.innerHeight;
+			currentIndex = i + 1;
+			window.scrollTo({
+				top: currentIndex * 0.93 * h,
+				behavior: 'smooth'
+			});
+			ableToScroll = false;
+		}
+		setTimeout(() => {
+			ableToScroll = true;
+		}, 1000);
+	});
+}
--- src/main/java/com/hgfluid/Foreignwebsite/entity/QueryEntity.java
+++ src/main/java/com/hgfluid/Foreignwebsite/entity/QueryEntity.java
@@ -1,18 +1,23 @@
 package com.hgfluid.Foreignwebsite.entity;
 
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 import java.io.Serializable;
 
 @Getter
 @Setter
 @ToString
 public class QueryEntity implements Serializable {
-    private Integer id;
-    private String name;
-    private String mail;
-    private String demand;
+
+	private static final long serialVersionUID = 3290749911204547116L;
+
+	private Integer id;
+
+	private String name;
+
+	private String mail;
+
+	private String demand;
+
 }
