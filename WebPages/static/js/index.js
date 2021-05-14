260ba6ac05f81455420581e8de4a056655933b37
守望时空33 <yanhanhui2001a@163.com>
2021-05-14 15:10:06 +0800

    v1.0.0

--- WebPages/static/js/index.js
+++ WebPages/static/js/index.js
@@ -40,7 +40,7 @@ for (let i = 0; i < menuBar.children.length; i++) {
 			ableToScroll = false;
 			setTimeout(() => {
 				ableToScroll = true;
-			}, 1000);
+			}, 200);
 		}
 	});
 }
@@ -56,52 +56,56 @@ document.querySelector('header .logo').addEventListener('click', (e) => {
 		ableToScroll = false;
 		setTimeout(() => {
 			ableToScroll = true;
-		}, 1000);
+		}, 100);
 	}
 });
 
-//手机端展开按钮
-let isMenuOpen = false;
-document.querySelector('header .unfoldButton').addEventListener('click', (e) => {
-	e.stopPropagation();
-	isMenuOpen = !isMenuOpen;
-	if (isMenuOpen) {
-		menuBar.style.display = 'flex';
-	} else {
-		menuBar.style.display = 'none';
-	}
-});
-document.querySelector('body').addEventListener('click', (e) => {
-	isMenuOpen = false;
-	menuBar.style.display = 'none';
-});
 
-//手机端滑动事件
-let startY, endY;
-document.querySelector('body').addEventListener('touchstart', (e) => {
-	startY = e.targetTouches[0].pageY;
-});
-
-document.querySelector('body').addEventListener('touchmove', (e) => {
-	if (ableToScroll) {
-		let h = window.innerHeight;
-		endY = e.targetTouches[0].pageY;
-		if (endY - startY < 0) {
-			if (currentIndex < 4) {
-				currentIndex++;
-			}
+if (window.innerWidth < 768) {
+	//手机端展开按钮
+	let isMenuOpen = false;
+	document.querySelector('header .unfoldButton').addEventListener('click', (e) => {
+		e.stopPropagation();
+		isMenuOpen = !isMenuOpen;
+		if (isMenuOpen) {
+			menuBar.style.display = 'flex';
 		} else {
-			if (currentIndex > 0) {
-				currentIndex--;
-			}
+			menuBar.style.display = 'none';
 		}
-		window.scrollTo({
-			top: currentIndex * 0.93 * h,
-			behavior: 'smooth'
-		});
-		ableToScroll = false;
-		setTimeout(() => {
-			ableToScroll = true;
-		}, 500);
-	}
-});
+	});
+
+	document.querySelector('body').addEventListener('click', (e) => {
+		isMenuOpen = false;
+		menuBar.style.display = 'none';
+	});
+
+	//手机端滑动事件
+	let startY, endY;
+	document.querySelector('body').addEventListener('touchstart', (e) => {
+		startY = e.targetTouches[0].pageY;
+	});
+
+	document.querySelector('body').addEventListener('touchmove', (e) => {
+		if (ableToScroll) {
+			let h = window.innerHeight;
+			endY = e.targetTouches[0].pageY;
+			if (endY - startY < 0) {
+				if (currentIndex < 4) {
+					currentIndex++;
+				}
+			} else {
+				if (currentIndex > 0) {
+					currentIndex--;
+				}
+			}
+			window.scrollTo({
+				top: currentIndex * 0.93 * h,
+				behavior: 'smooth'
+			});
+			ableToScroll = false;
+			setTimeout(() => {
+				ableToScroll = true;
+			}, 500);
+		}
+	});
+}
